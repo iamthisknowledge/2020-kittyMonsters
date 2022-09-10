503ccd08a5e51261279757afaf6919cf90847fc1
rodrigo <rodrigo@pop-os.localdomain>
2022-09-10 23:21:22 +0200

    2020 was the last time I touched this. Cards displaying a few kitties. Used API call to get all infos and was made with React.js.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState( {monsters: users }))
+  }
+  render() {
+    return(
+      <div className="App">
+      <CardList monsters={this.state.monsters} />   
+    </div>
+    );
+  }
+
+}
+
+export default App;
